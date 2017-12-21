e845578017ff66c374c66440a4b9c2bd9a5217ef
Achala Kavinda <achalakavinda25r@gmail.com>
2017-12-20 19:52:02 -0800

    map card

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,6 +11,8 @@ export class HomePage {
 
     @ViewChild('map') mapElement: ElementRef;
 
+    Items=[{ok:'1'},{ok:2}];
+
   map: any;
 
   constructor(public navCtrl: NavController) {
@@ -26,13 +28,44 @@ export class HomePage {
  
  let mapOptions = {
         center:{lat: -34.397, lng: 150.644},
-        zoom: 15,
+        zoom: 10,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       }
     
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+      setTimeout(()=>{
+        console.log('call');
+        this.addMarker();
+      },1000);
     
    }
+
+   addMarker(){
+ 
+  let marker = new google.maps.Marker({
+    map: this.map,
+    animation: google.maps.Animation.DROP,
+    position: this.map.getCenter()
+  });
+ 
+  let content = "<h4>Information!</h4>";         
+ 
+  this.addInfoWindow(marker, content);
+ 
+}
+
+addInfoWindow(marker, content){
+ 
+  let infoWindow = new google.maps.InfoWindow({
+    content: content
+  });
+ 
+  google.maps.event.addListener(marker, 'click', () => {
+    infoWindow.open(this.map, marker);
+  });
+ 
+}
  
    
 
